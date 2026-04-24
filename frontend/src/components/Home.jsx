018982bbbb25b9c65998eddc995c11d44f53792e
Aditya Kumar <adityakumar.455223@gmail.com>
2026-04-24 12:10:50 +0530

    Initial commit - Job Portal project

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
+import LatestJobs from './LatestJobs'
+import Footer from './shared/Footer'
+import useGetAllJobs from '@/hooks/useGetAllJobs'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+const Home = () => {
+  useGetAllJobs();
+  const { user } = useSelector(store => store.auth);
+  const navigate = useNavigate();
+  // Removed redirect to allow seamless switching and viewing of landing page
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <CategoryCarousel />
+      <LatestJobs />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
